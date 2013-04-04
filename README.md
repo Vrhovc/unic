b91026f5970f141e2944def218dcd5a9d01d078f
Ethan Piliavin <ethan@piliavin.com>
2013-04-03 20:44:43 -0700

    First Day

---
+++ README.md
@@ -0,0 +1,10 @@
+# unic :: underscores+unsemantic
+## a bare naked wordpress starter theme
+
+This is my custom WordPress theme starter. Starting with a download of Underscores (http://underscores.me/), I integrated the Unsemantic Grid (http://unsemantic.com/). Lastly, I added a general.js file to the header.
+
+##ToDo
+todo
+
+##Credits
+todo
---
+++ functions.php
@@ -0,0 +1,151 @@
+<?php
+/**
+ * unic functions and definitions
+ *
+ * @package unic
+ */
+
+/**
+ * Set the content width based on the theme's design and stylesheet.
+ */
+if ( ! isset( $content_width ) )
+	$content_width = 640; /* pixels */
+
+/*
+ * Load Jetpack compatibility file.
+ */
+require( get_template_directory() . '/inc/jetpack.php' );
+
+if ( ! function_exists( 'unic_setup' ) ) :
+/**
+ * Sets up theme defaults and registers support for various WordPress features.
+ *
+ * Note that this function is hooked into the after_setup_theme hook, which runs
+ * before the init hook. The init hook is too late for some features, such as indicating
+ * support post thumbnails.
+ */
+function unic_setup() {
+
+	/**
+	 * Custom template tags for this theme.
+	 */
+	require( get_template_directory() . '/inc/template-tags.php' );
+
+	/**
+	 * Custom functions that act independently of the theme templates
+	 */
+	require( get_template_directory() . '/inc/extras.php' );
+
+	/**
+	 * Customizer additions
+	 */
+	require( get_template_directory() . '/inc/customizer.php' );
+
+	/**
+	 * Make theme available for translation
+	 * Translations can be filed in the /languages/ directory
+	 * If you're building a theme based on unic, use a find and replace
+	 * to change 'unic' to the name of your theme in all the template files
+	 */
+	load_theme_textdomain( 'unic', get_template_directory() . '/languages' );
+
+	/**
+	 * Add default posts and comments RSS feed links to head
+	 */
+	add_theme_support( 'automatic-feed-links' );
+
+	/**
+	 * Enable support for Post Thumbnails
+	 */
+	add_theme_support( 'post-thumbnails' );
+
+	/**
+	 * This theme uses wp_nav_menu() in one location.
+	 */
+	register_nav_menus( array(
+		'primary' => __( 'Primary Menu', 'unic' ),
+	) );
+
+	/**
+	 * Enable support for Post Formats
+	 */
+	add_theme_support( 'post-formats', array( 'aside', 'image', 'video', 'quote', 'link' ) );
+}
+endif; // unic_setup
+add_action( 'after_setup_theme', 'unic_setup' );
+
+/**
+ * Setup the WordPress core custom background feature.
+ *
+ * Use add_theme_support to register support for WordPress 3.4+
+ * as well as provide backward compatibility for WordPress 3.3
+ * using feature detection of wp_get_theme() which was introduced
+ * in WordPress 3.4.
+ *
+ * @todo Remove the 3.3 support when WordPress 3.6 is released.
+ *
+ * Hooks into the after_setup_theme action.
+ */
+function unic_register_custom_background() {
+	$args = array(
+		'default-color' => 'ffffff',
+		'default-image' => '',
+	);
+
+	$args = apply_filters( 'unic_custom_background_args', $args );
+
+	if ( function_exists( 'wp_get_theme' ) ) {
+		add_theme_support( 'custom-background', $args );
+	} else {
+		define( 'BACKGROUND_COLOR', $args['default-color'] );
+		if ( ! empty( $args['default-image'] ) )
+			define( 'BACKGROUND_IMAGE', $args['default-image'] );
+		add_custom_background();
+	}
+}
+add_action( 'after_setup_theme', 'unic_register_custom_background' );
+
+/**
+ * Register widgetized area and update sidebar with default widgets
+ */
+function unic_widgets_init() {
+	register_sidebar( array(
+		'name'          => __( 'Sidebar', 'unic' ),
+		'id'            => 'sidebar-1',
+		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</aside>',
+		'before_title'  => '<h1 class="widget-title">',
+		'after_title'   => '</h1>',
+	) );
+}
+add_action( 'widgets_init', 'unic_widgets_init' );
+
+/**
+ * Enqueue scripts and styles
+ */
+function unic_scripts() {
+
+	wp_enqueue_style( 'unsemantic-grid', get_template_directory_uri() . '/css/unsemantic-grid-responsive-no-ie7.css');		
+	
+	wp_enqueue_style( 'unic-style', get_stylesheet_uri() );
+	
+	wp_enqueue_script( 'unic-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
+
+	wp_enqueue_script( 'unic-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
+	
+	wp_enqueue_script( 'unic-general', get_template_directory_uri() . '/js/general.js', array('jquery'), '20130413', false );
+
+	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
+		wp_enqueue_script( 'comment-reply' );
+	}
+
+	if ( is_singular() && wp_attachment_is_image() ) {
+		wp_enqueue_script( 'unic-keyboard-image-navigation', get_template_directory_uri() . '/js/keyboard-image-navigation.js', array( 'jquery' ), '20120202' );
+	}
+}
+add_action( 'wp_enqueue_scripts', 'unic_scripts' );
+
+/**
+ * Implement the Custom Header feature
+ */
+//require( get_template_directory() . '/inc/custom-header.php' );
