c6ef762a602478dc74c25d59a755209ba37d9bdf
Ethan Piliavin <ethan@piliavin.com>
2013-11-21 13:59:08 -0800

    Hide annoying woothemes notices

--- inc/woocommerce.php
+++ inc/woocommerce.php
@@ -4,4 +4,7 @@
 //Declare WooCommerce Support to prevent message in admin
 add_theme_support( 'woocommerce' );
 
+//Hide annoying woothemes notices
+remove_action( 'admin_notices', 'woothemes_updater_notice' );
+
 ?>
