5780b2250b117734b503bd4fa526748868069424
Ethan Piliavin <ethan@piliavin.com>
2013-11-28 00:04:33 -0800

    Add Magnific Popup

--- js/general.js
+++ js/general.js
@@ -1,4 +1,10 @@
 jQuery(document).ready(function($) {
   // Code using $ as usual goes here.
   
+    // Initialize Popup
+    $('.woocommerce-main-image, .thumbnails a').magnificPopup({
+		type:'image',
+		closeOnContentClick:true
+	});
+  
 });
