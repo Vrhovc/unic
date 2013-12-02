60af07f4d1b3b2854604a6735e901219888e9dc5
Ethan Piliavin <ethan@piliavin.com>
2013-12-02 12:25:41 -0800

    fix select menu warning message

--- inc/select-menu.php
+++ inc/select-menu.php
@@ -1,22 +1,22 @@
 <?php
 
 class Walker_Nav_Menu_Select extends Walker_Nav_Menu {
-	function start_lvl(&$output, $depth){
+	function start_lvl(&$output, $depth = 0, $args = array()){
 		$indent = str_repeat("\t", $depth); // don't output children opening tag (`<ul>`)
 	}
  
-	function end_lvl(&$output, $depth){
+	function end_lvl(&$output, $depth = 0, $args = array()){
 		$indent = str_repeat("\t", $depth); // don't output children closing tag
 	}
  
-	function start_el(&$output, $item, $depth, $args) {
-		$indent = str_repeat("-", $depth);
+	function start_el(&$output, $item, $depth = 0, $args = array(), $current_object_id = 0) {
+		$indent = str_repeat("--", $depth);
 		$selected = ( in_array("current-menu-item", $item->classes) ) ? ' selected' : '';
  		$url = '#' !== $item->url ? $item->url : '';
  		$output .= '<option value="' . $url . '"'.$selected.'>' . $indent.$item->title;
 	}	
  
-	function end_el(&$output, $item, $depth){
+	function end_el(&$output, $item, $depth = 0, $args = array()){
 		$output .= "</option>\n"; // replace closing </li> with the option tag
 	}
 }
@@ -34,4 +34,4 @@ function wp_nav_menu_select($args = array())
 	
 	wp_nav_menu( $args );
 }
-?>	
+?>
