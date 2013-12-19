7c4ff65e013d32056b30b897dbbd408b4fbe0f85
ethanpil <ethan@piliavin.com>
2013-12-19 12:10:17 -0800

    Move additional scripts to page footer.

--- inc/styles-scripts.php
+++ inc/styles-scripts.php
@@ -5,15 +5,15 @@ function unic_scripts() {
 	wp_enqueue_style( 'unic-style', get_stylesheet_uri() );
 
 	//Javascripts
-	wp_enqueue_script( 'unic-general', get_template_directory_uri() . '/js/general.js', array('jquery'), '20131202', false );
-	wp_enqueue_script( 'magnific-popup', get_template_directory_uri() . '/js/third-party/magnific-popup.js', array('jquery','unic-general'), '20131202', false );
+	wp_enqueue_script( 'unic-general', get_template_directory_uri() . '/js/general.js', array('jquery'), '20131202', true );
+	wp_enqueue_script( 'magnific-popup', get_template_directory_uri() . '/js/third-party/magnific-popup.js', array('jquery','unic-general'), '20131202', true );
 	
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
 
 	if ( is_singular() && wp_attachment_is_image() ) {
-		wp_enqueue_script( 'unic-keyboard-image-navigation', get_template_directory_uri() . '/js/keyboard-image-navigation.js', array( 'jquery' ), '20131202' );
+		wp_enqueue_script( 'unic-keyboard-image-navigation', get_template_directory_uri() . '/js/keyboard-image-navigation.js', array( 'jquery' ), '20131202', true );
 	}
 
 	
