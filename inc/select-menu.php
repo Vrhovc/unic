016f2dc9cc703c3bf788c7fa11dad1931250b562
ethanpil <ethan@piliavin.com>
2014-01-21 00:11:16 -0500

    Fix bug that output unnecessary </option> tag

--- inc/select-menu.php
+++ inc/select-menu.php
@@ -1,23 +1,30 @@
 <?php
 
 class Walker_Nav_Menu_Select extends Walker_Nav_Menu {
-	function start_lvl(&$output, $depth = 0, $args = array()){
+	function start_lvl(&$output, $depth){
 		$indent = str_repeat("\t", $depth); // don't output children opening tag (`<ul>`)
 	}
  
-	function end_lvl(&$output, $depth = 0, $args = array()){
+	function end_lvl(&$output, $depth){
 		$indent = str_repeat("\t", $depth); // don't output children closing tag
 	}
  
-	function start_el(&$output, $item, $depth = 0, $args = array(), $current_object_id = 0) {
-		$indent = str_repeat("--", $depth);
+	function start_el(&$output, $item, $depth, $args) {
+		$indent = str_repeat("-", $depth);
 		$selected = ( in_array("current-menu-item", $item->classes) ) ? ' selected' : '';
  		$url = '#' !== $item->url ? $item->url : '';
  		$output .= '<option value="' . $url . '"'.$selected.'>' . $indent.$item->title;
+		if( 0 == $depth ) $output .=  "</option>\n";
+		
 	}	
  
-	function end_el(&$output, $item, $depth = 0, $args = array()){
-		$output .= "</option>\n"; // replace closing </li> with the option tag
+	function end_el(&$output, $item, $depth){
+				
+		//Since the walker function was designed to nest list items, we have to test to see if we are adding an unnecessary closing option.
+		$test_string = trim(preg_replace('/\s+/', ' ', substr($output, -10)));
+		if ($test_string != "</option>") {
+			$output .= "</option>\n"; // replace closing </li> with the option tag
+		}
 	}
 }
 
